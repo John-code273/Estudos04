f6bcee62bea3b5e912e7f232c3472c29fb0c0347
john <john.silvaromao@gmail.com>
2026-01-12 11:43:18 -0300

    Estudo e POO. Calculo de dolar. metodo statico.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-static void main() {
-    Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
 
-    System.out.print("Valor do dolar: ");
-    Conversao.dolar = sc.nextDouble();
-    System.out.print("quantos dolar voce que compra: ");
-    Conversao.compra = sc.nextDouble();
-    System.out.println("o seu valor em reais foi: "+ Conversao.calculo());
+        System.out.print("Valor do dolar: ");
+        Conversao.dolar = sc.nextDouble();
+        System.out.print("quantos dolar voce que compra: ");
+        Conversao.compra = sc.nextDouble();
+        System.out.println("o seu valor em reais foi: " + Conversao.calculo());
 
+    }
 }
